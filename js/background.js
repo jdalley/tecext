994ac1ce860f281132827a2c24b3dc6fe03944bb
Jeff Dalley <jeffdalley@gmail.com>
2021-11-28 21:18:49 -0500

    Added another entangle parse for being snared

--- js/background.js
+++ js/background.js
@@ -482,7 +482,8 @@ function combatGlobals(data) {
 
 	// Handle entagled weapon
 	if (data.indexOf("You cannot attack with an entangled weapon") >= 0 
-		|| data.indexOf("You cannot use that action while grappling") >= 0) {
+		|| data.indexOf("You cannot use that action while grappling") >= 0
+		|| data.indexOf("You are unable to do that, as you are snared by") >= 0) {
 		sendDelayedCommands([
 			`free`,
 			`${commandList[currentCmdIndex].command} ${target}`,
